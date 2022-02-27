8a097adef8ea5902efb94b9dc282bc2301508bd3
Minami Tomonobu <minami@edanoue.com>
2022-02-27 14:57:57 +0900

    added maketx script for HDRI images

---
+++ tools/maketx_for_hdri.bat
@@ -0,0 +1,41 @@
+@ECHO off
+
+@REM set Maketx path
+@SET "MAKETX=%userprofile%/ktoa/ktoa-4.0.0.2-kat4.5-windows/bin/maketx.exe"
+
+@REM # Options
+
+@REM ## Input Image Options
+@SET "IN=../textures/hdri/studio_02.ACES - ACEScg.2k.exr"
+@SET "IN_COLORSPACE=ACES - ACEScg"
+
+@REM ## Output Image Options
+@REM Select output format: tiff, OpenEXR
+@SET "OUT_FORMAT=OpenEXR"
+@REM Select output data bit-depth: uint8, sint8, uint16, sint16, half, float
+@SET "OUT_BITDEPTHT=half"
+@SET "OUT_COLORSPACE=ACES - ACEScg"
+@SET "OUT=../textures/hdri/tiled.exr"
+
+@REM ## Maketx Options
+@SET "TILESIZE=64 64"
+
+@REM OCIO
+set "OCIO=%userprofile%/Documents/ocio/aces_1.2/config.ocio"
+
+
+@REM run Maketx
+"%MAKETX%"^
+    -v --runstats^
+    -o "%OUT%"^
+    --format "%OUT_FORMAT%"^
+    -d "%OUT_BITDEPTHT%"^
+    --tile "%TILESIZE%"^
+    --separate^
+    --colorconfig "%OCIO%"^
+    --colorconvert "%IN_COLORSPACE%" "%OUT_COLORSPACE%"^
+    --filter lanczos3^
+    --envlatl^
+    "%IN%"
+
+pause
