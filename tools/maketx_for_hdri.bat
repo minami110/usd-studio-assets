8b9ab21307fb359542909d04d9954f8ea770498f
Minami Tomonobu <minami@edanoue.com>
2022-02-27 15:23:52 +0900

    fix format to float

--- tools/maketx_for_hdri.bat
+++ tools/maketx_for_hdri.bat
@@ -6,14 +6,14 @@
 @REM # Options
 
 @REM ## Input Image Options
-@SET "IN=../textures/hdri/studio_02.ACES - ACEScg.2k.exr"
+@SET "IN=../textures/hdri/wht.2k.acescg.exr"
 @SET "IN_COLORSPACE=ACES - ACEScg"
 
 @REM ## Output Image Options
 @REM Select output format: tiff, OpenEXR
 @SET "OUT_FORMAT=OpenEXR"
 @REM Select output data bit-depth: uint8, sint8, uint16, sint16, half, float
-@SET "OUT_BITDEPTHT=half"
+@SET "OUT_BITDEPTHT=float"
 @SET "OUT_COLORSPACE=ACES - ACEScg"
 @SET "OUT=../textures/hdri/tiled.exr"
 
